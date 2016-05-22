45b4e307c17b541fbe787167f9d8196a0df5b816
Flibio <flibio@users.noreply.github.com>
2016-05-22 09:20:50 -0600

    Update for Sponge API 4

--- src/me/flibio/winterwonderland/Main.java
+++ src/me/flibio/winterwonderland/Main.java
@@ -1,8 +1,8 @@
 package me.flibio.winterwonderland;
 
+import com.google.inject.Inject;
 import me.flibio.winterwonderland.FileManager.FileType;
 import ninja.leaping.configurate.ConfigurationNode;
-
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.GameRegistry;
@@ -24,11 +24,9 @@ import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.scheduler.Scheduler;
-import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
-
-import com.google.inject.Inject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,159 +34,157 @@ import java.util.Calendar;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
-@Plugin(id = "WinterWonderland", name = "Winter Wonderland", version = "1.1.0")
+@Plugin(id = "winterwonderland", name = "Winter Wonderland", version = "1.1.1")
 public class Main {
-	
-	public static Main access;
-	
-	@Inject
-	Logger logger;
-	
-	@Inject
-	Game game;
-	
-	private GameRegistry registery;
-	private FileManager fileManager;
-	private Scheduler scheduler;
-	protected ConfigurationNode playerData;
-	
-	private ArrayList<Location<World>> snowLocs = new ArrayList<Location<World>>();
 
-	private ArrayList<BlockType> blockedTypes = new ArrayList<>(Arrays.asList(BlockTypes.WOODEN_SLAB,BlockTypes.STONE_SLAB,BlockTypes.STONE_SLAB2,
-			BlockTypes.STANDING_SIGN,BlockTypes.WALL_SIGN));
-	
-	private boolean enabled = false;
-	
-	@Listener
-	public void onServerStart(GameInitializationEvent event) {
-		registery = game.getRegistry();
-		scheduler = game.getScheduler();
-		fileManager = new FileManager(logger);
-		
-		access = this;
-		
-		//File generation
-		fileManager.generateFolder("config/WinterWonderland");
-		fileManager.generateFile("config/WinterWonderland/config.conf");
-		fileManager.generateFile("config/WinterWonderland/data.conf");
-		fileManager.loadFile(FileType.DATA);
-		fileManager.loadFile(FileType.CONFIGURATION);
-		fileManager.testDefault("ignore-date", "disabled");
-		
-		fileManager.loadFile(FileType.CONFIGURATION);
-		if(fileManager.getConfigValue("ignore-date").equalsIgnoreCase("enabled")) {
-			enabled = true;
-		}
-		
-		CommandSpec toggleCommand = CommandSpec.builder()
-			    .description(Texts.of("Toggle snow placement"))
-			    .permission("winter.toggle")
-			    .executor(new ToggleCommand())
-			    .build();
-		game.getCommandManager().register(this, toggleCommand, "winter");
-		
-		playerData = fileManager.getFile(FileType.DATA);
-		
-		//Check for the correct date
-		if(!enabled) {
-			scheduler.createTaskBuilder().execute(r -> {
-				enabled = isEnabled();
-			}).interval(10, TimeUnit.MINUTES).submit(this);
-		}
-	}
-	
-	@Listener
-	public void onServerStop(GameStoppingServerEvent event) {
-		fileManager.saveFile(FileType.DATA, playerData);
-	}
-	
-	@Listener
-	public void onPlayerJoin(ClientConnectionEvent.Join event) {
-		Player player = event.getTargetEntity();
-		String uuid = player.getUniqueId().toString();
-		if(playerData.getNode(uuid)==null) {
-			playerData.getNode(uuid).setValue(true);
-		}
-	}
-	
-	@Listener
-	public void onPlayerMove(DisplaceEntityEvent.Move.TargetPlayer event) {
-		if(enabled) {
-			Player player = event.getTargetEntity();
-			String uuid = player.getUniqueId().toString();
-			if(playerData.getNode(uuid)!=null&&playerData.getNode(uuid).getBoolean()) {
-				Location<World> loc = player.getLocation();
-				if(loc.getBlockType().equals(BlockTypes.AIR)&&!loc.add(0, -1, 0).getBlockType().equals(BlockTypes.AIR)) {
-					Optional<PassableProperty> passableOptional = loc.add(0, 0, 0).getProperty(PassableProperty.class);
-					Optional<SolidCubeProperty> solidOptional = loc.add(0, -1, 0).getProperty(SolidCubeProperty.class);
-					if(solidOptional.isPresent()) {
-						if(!solidOptional.get().getValue()) {
-							return;
-						}
-					}
-					if(passableOptional.isPresent()) {
-						if(!passableOptional.get().getValue()) {
-							return;
-						}
-					}
-					if(blockedTypes.contains(loc.add(0, -1, 0).getBlockType())||blockedTypes.contains(loc.getBlockType())) {
-						return;
-					}
-					if(loc.add(0, -1, 0).get(Keys.STAIR_SHAPE).isPresent()||loc.get(Keys.STAIR_SHAPE).isPresent()) {
-						return;
-					}
-					loc.setBlockType(BlockTypes.SNOW_LAYER);
-					Location<World> rounded = new Location<World>(loc.getExtent(),loc.getBlockX(),loc.getBlockY(),loc.getBlockZ());
-					snowLocs.add(rounded);
-					ParticleEffect effect = registery.createBuilder(ParticleEffect.Builder.class).type(ParticleTypes.SNOWBALL).count(50).build();
-					player.spawnParticles(effect, loc.getPosition(), 32);	
-					scheduler.createTaskBuilder().execute(r -> {
-						snowLocs.remove(loc);
-						if(loc.getBlockType().equals(BlockTypes.SNOW_LAYER)) {
-							loc.setBlockType(BlockTypes.AIR);
-						}
-					}).delayTicks(50).submit(this);
-				}
-			}
-		}
-	}
-	
-	@Listener
-	public void onItemDrop(DropItemEvent.Pre event) {
-		if(enabled) {
-			Optional<BlockSnapshot> blockOptional = event.getCause().first(BlockSnapshot.class);
-			if(blockOptional.isPresent()) {
-				BlockSnapshot block = blockOptional.get();
-				if(block.getState().getType().equals(BlockTypes.SNOW_LAYER)) {
-					if(block.getLocation().isPresent()) {
-						if(snowLocs.contains(block.getLocation().get())) {
-							event.setCancelled(true);
-							snowLocs.remove(block.getLocation().get());
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	private boolean isEnabled() {
-		int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
-		int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-		logger.info("Current Date: "+(currentMonth+1)+"-"+currentDay);
-		if(currentMonth==11||currentMonth==0) {
-			if(currentMonth==11) {
-				if(currentDay>=18) {
-					logger.info("Winter Wonderland is enabled! The last day it will be enabled is 1-1.");
-					return true;
-				}
-			} else if(currentMonth==0) {
-				if(currentDay<=1) {
-					logger.info("Winter Wonderland is enabled! The last day it will be enabled is 1-1.");
-					return true;
-				}
-			}
-		}
-		logger.info("Winter Wonderland is disabled! It will be enabled on 12-18.");
-		return false;
-	}
+    public static Main access;
+
+    @Inject Logger logger;
+
+    @Inject Game game;
+
+    private GameRegistry registery;
+    private FileManager fileManager;
+    private Scheduler scheduler;
+    protected ConfigurationNode playerData;
+
+    private ArrayList<Location<World>> snowLocs = new ArrayList<Location<World>>();
+
+    private ArrayList<BlockType> blockedTypes = new ArrayList<>(Arrays.asList(BlockTypes.WOODEN_SLAB, BlockTypes.STONE_SLAB, BlockTypes.STONE_SLAB2,
+            BlockTypes.STANDING_SIGN, BlockTypes.WALL_SIGN));
+
+    private boolean enabled = false;
+
+    @Listener
+    public void onServerStart(GameInitializationEvent event) {
+        registery = game.getRegistry();
+        scheduler = game.getScheduler();
+        fileManager = new FileManager(logger);
+
+        access = this;
+
+        // File generation
+        fileManager.generateFolder("config/WinterWonderland");
+        fileManager.generateFile("config/WinterWonderland/config.conf");
+        fileManager.generateFile("config/WinterWonderland/data.conf");
+        fileManager.loadFile(FileType.DATA);
+        fileManager.loadFile(FileType.CONFIGURATION);
+        fileManager.testDefault("ignore-date", "disabled");
+
+        fileManager.loadFile(FileType.CONFIGURATION);
+        if (fileManager.getConfigValue("ignore-date").equalsIgnoreCase("enabled")) {
+            enabled = true;
+        }
+
+        CommandSpec toggleCommand = CommandSpec.builder()
+                .description(Text.of("Toggle snow placement"))
+                .permission("winter.toggle")
+                .executor(new ToggleCommand())
+                .build();
+        game.getCommandManager().register(this, toggleCommand, "winter");
+
+        playerData = fileManager.getFile(FileType.DATA);
+
+        // Check for the correct date
+        if (!enabled) {
+            scheduler.createTaskBuilder().execute(r -> {
+                enabled = isEnabled();
+            }).interval(10, TimeUnit.MINUTES).submit(this);
+        }
+    }
+
+    @Listener
+    public void onServerStop(GameStoppingServerEvent event) {
+        fileManager.saveFile(FileType.DATA, playerData);
+    }
+
+    @Listener
+    public void onPlayerJoin(ClientConnectionEvent.Join event) {
+        Player player = event.getTargetEntity();
+        String uuid = player.getUniqueId().toString();
+        if (playerData.getNode(uuid) == null) {
+            playerData.getNode(uuid).setValue(true);
+        }
+    }
+
+    @Listener
+    public void onPlayerMove(DisplaceEntityEvent.Move.TargetPlayer event) {
+        if (enabled) {
+            Player player = event.getTargetEntity();
+            String uuid = player.getUniqueId().toString();
+            if (playerData.getNode(uuid) != null && playerData.getNode(uuid).getBoolean()) {
+                Location<World> loc = player.getLocation();
+                if (loc.getBlockType().equals(BlockTypes.AIR) && !loc.add(0, -1, 0).getBlockType().equals(BlockTypes.AIR)) {
+                    Optional<PassableProperty> passableOptional = loc.add(0, 0, 0).getProperty(PassableProperty.class);
+                    Optional<SolidCubeProperty> solidOptional = loc.add(0, -1, 0).getProperty(SolidCubeProperty.class);
+                    if (solidOptional.isPresent()) {
+                        if (!solidOptional.get().getValue()) {
+                            return;
+                        }
+                    }
+                    if (passableOptional.isPresent()) {
+                        if (!passableOptional.get().getValue()) {
+                            return;
+                        }
+                    }
+                    if (blockedTypes.contains(loc.add(0, -1, 0).getBlockType()) || blockedTypes.contains(loc.getBlockType())) {
+                        return;
+                    }
+                    if (loc.add(0, -1, 0).get(Keys.STAIR_SHAPE).isPresent() || loc.get(Keys.STAIR_SHAPE).isPresent()) {
+                        return;
+                    }
+                    loc.setBlockType(BlockTypes.SNOW_LAYER);
+                    Location<World> rounded = new Location<World>(loc.getExtent(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+                    snowLocs.add(rounded);
+                    ParticleEffect effect = registery.createBuilder(ParticleEffect.Builder.class).type(ParticleTypes.SNOWBALL).count(50).build();
+                    player.spawnParticles(effect, loc.getPosition(), 32);
+                    scheduler.createTaskBuilder().execute(r -> {
+                        snowLocs.remove(loc);
+                        if (loc.getBlockType().equals(BlockTypes.SNOW_LAYER)) {
+                            loc.setBlockType(BlockTypes.AIR);
+                        }
+                    }).delayTicks(50).submit(this);
+                }
+            }
+        }
+    }
+
+    @Listener
+    public void onItemDrop(DropItemEvent.Pre event) {
+        if (enabled) {
+            Optional<BlockSnapshot> blockOptional = event.getCause().first(BlockSnapshot.class);
+            if (blockOptional.isPresent()) {
+                BlockSnapshot block = blockOptional.get();
+                if (block.getState().getType().equals(BlockTypes.SNOW_LAYER)) {
+                    if (block.getLocation().isPresent()) {
+                        if (snowLocs.contains(block.getLocation().get())) {
+                            event.setCancelled(true);
+                            snowLocs.remove(block.getLocation().get());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isEnabled() {
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+        int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        logger.info("Current Date: " + (currentMonth + 1) + "-" + currentDay);
+        if (currentMonth == 11 || currentMonth == 0) {
+            if (currentMonth == 11) {
+                if (currentDay >= 18) {
+                    logger.info("Winter Wonderland is enabled! The last day it will be enabled is 1-1.");
+                    return true;
+                }
+            } else if (currentMonth == 0) {
+                if (currentDay <= 1) {
+                    logger.info("Winter Wonderland is enabled! The last day it will be enabled is 1-1.");
+                    return true;
+                }
+            }
+        }
+        logger.info("Winter Wonderland is disabled! It will be enabled on 12-18.");
+        return false;
+    }
 }
--- src/me/flibio/winterwonderland/ToggleCommand.java
+++ src/me/flibio/winterwonderland/ToggleCommand.java
@@ -7,37 +7,36 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.text.format.TextColors;
 
 public class ToggleCommand implements CommandExecutor {
-	
-	private Text prefix = Texts.of(TextColors.AQUA,"Winter Wonderland » ",TextColors.WHITE);
-	
-	@Override
-	public CommandResult execute(CommandSource source, CommandContext args)
-			throws CommandException {
-		
-		if(!(source instanceof Player)) {
-			source.sendMessage(Texts.builder("You must be a player to use /winter!").color(TextColors.RED).build());
-			return CommandResult.success();
-		}
-		
-		Player player = (Player) source;
-		String uuid = player.getUniqueId().toString();
-		if(Main.access.playerData.getNode(uuid)==null) {
-			source.sendMessage(Texts.builder("An error has occurred!").color(TextColors.RED).build());
-			return CommandResult.success();
-		} else {
-			boolean value = Main.access.playerData.getNode(uuid).getBoolean();
-			Main.access.playerData.getNode(uuid).setValue(!value);
-			if(value) {
-				player.sendMessage(prefix.builder().append(Texts.of(TextColors.RED,"Turned off ",TextColors.WHITE,"snow placement!")).build());
-			} else {
-				player.sendMessage(prefix.builder().append(Texts.of(TextColors.GREEN,"Turned on ",TextColors.WHITE,"snow placement!")).build());
-			}
-		}
-		
-		return CommandResult.success();
-	}
+
+    private Text prefix = Text.of(TextColors.AQUA, "Winter Wonderland: ", TextColors.WHITE);
+
+    @Override
+    public CommandResult execute(CommandSource source, CommandContext args)
+            throws CommandException {
+
+        if (!(source instanceof Player)) {
+            source.sendMessage(Text.builder("You must be a player to use /winter!").color(TextColors.RED).build());
+            return CommandResult.success();
+        }
+
+        Player player = (Player) source;
+        String uuid = player.getUniqueId().toString();
+        if (Main.access.playerData.getNode(uuid) == null) {
+            source.sendMessage(Text.builder("An error has occurred!").color(TextColors.RED).build());
+            return CommandResult.success();
+        } else {
+            boolean value = Main.access.playerData.getNode(uuid).getBoolean();
+            Main.access.playerData.getNode(uuid).setValue(!value);
+            if (value) {
+                player.sendMessage(prefix.toBuilder().append(Text.of(TextColors.RED, "Turned off ", TextColors.WHITE, "snow placement!")).build());
+            } else {
+                player.sendMessage(prefix.toBuilder().append(Text.of(TextColors.GREEN, "Turned on ", TextColors.WHITE, "snow placement!")).build());
+            }
+        }
+
+        return CommandResult.success();
+    }
 }
